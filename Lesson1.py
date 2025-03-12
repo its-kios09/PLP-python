b1f7a4deebe1cfbcafee9afca1da2dbcc9cb85aa
its-kios09 <legacyitsolution@gmail.com>
2025-03-12 17:39:18 +0300

    (feat) finished lesson one about python variables

---
+++ Lesson1.py
@@ -0,0 +1,15 @@
+# Understanding python variables
+site_name = 'Power learn project'
+print(site_name)
+
+#assiging a new value to site_name
+site_name = 'apple.com'
+print(site_name)
+
+# this behavior is the reason why the python is a type-inferred language, you don't have to assign a variable type
+
+#Assigning the multiple variables and multiple values
+a, b, c = 2, 3.5, 'Hello world!'
+print(a)
+print(b)
+print(c)
