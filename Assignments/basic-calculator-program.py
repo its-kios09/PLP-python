5dd3417e8648cd26a180865f8fd8b517d694925a
its-kios09 <legacyitsolution@gmail.com>
2025-03-13 00:34:54 +0300

    (feat) added a class calculator for arithmetic operation

---
+++ Assignments/basic-calculator-program.py
@@ -0,0 +1,27 @@
+# Instructions:
+#
+# Basic Calculator Program
+#
+# Create a simple Python program that asks the user to input two numbers and a mathematical operation (addition, subtraction, multiplication, or division).
+# Perform the operation based on the user's input and print the result.
+# Example: If a user inputs 10, 5, and +, your program should display 10 + 5 = 15.
+
+class Calculator:
+    def __init__(self, a: int, b: int) -> None:
+        self.a = a
+        self.b = b
+        
+    def addition(self) -> int:
+        return  self.a + self.b
+    def subtraction(self) -> int:
+        return self.a - self.b
+    def multiplication(self) -> int:
+        return self.a * self.b
+    def division(self) -> float:
+        return self.a / self.b
+
+calculation = Calculator(10, 5)
+print('addition', calculation.addition())
+print('subtraction', calculation.subtraction())
+print('multiplication', calculation.multiplication())
+print('division', calculation.division())
